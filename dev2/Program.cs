65140f4069ba53a941173c252e0d1b5c0f25f4eb
NataSola <nsol2009@yandex.ru>
2021-11-02 10:31:29 +0300

    Add dev2 task 1

---
+++ dev2/Program.cs
@@ -0,0 +1,38 @@
+﻿// Имеется числовой массив A заполненный числами из отрезка [minValue; maxValue]. 
+// Создать на его основе масив B, отбрасывая те, которые нарушают порядок
+// 1-возрастания, 2-элементы больше 8, 3-знакочередование
+
+
+void FillArray(int[] collection)    // Создать массив А со случайными значениями от -100 до 100
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(-100, 101);
+        index++;
+    }
+}
+
+void PrintArray(int[] coll)    // Напечатать массив A
+{
+    int count = coll.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(coll[position] + " ");
+        position++;
+    }
+}
+
+
+int[] A = new int[25];
+
+//int current = A[2];
+//Console.WriteLine(current + " ");
+
+FillArray(A);
+PrintArray(A);
+
+
+
---
+++ dev22/Program.cs
@@ -0,0 +1,31 @@
+﻿int index = 0;
+int[] A = new int[25];
+while (index < 25)
+{
+    A[index] = new Random().Next(-25, 26);
+    index++;
+}
+
+index = 0;
+while (index < 25)
+{
+    int val = A[index];
+    Console.Write(val + " ");
+    index++;
+}
+Console.WriteLine();
+
+int current = A[0];
+Console.Write(current + " ");
+index = 1;
+while (index <= 24)
+{
+    if (A[index] > current)
+    {
+        current = A[index];
+        Console.Write(current + " ");
+        //index++;
+    }
+    //Console.Write(A[index]);
+    index++;
+}
